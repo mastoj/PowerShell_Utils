1bd6203bd82f82a68ea21ea64232f47b350bb153
Tomas Jansson <tomas.jansson@gmail.com>
2014-01-23 14:31:35 +0100

    backup script

---
+++ RemoteBackup.ps1
@@ -0,0 +1,24 @@
+
+$computer = read-host 'Enter server name'
+$database = read-host 'Enter database name'
+$backupName = read-host 'Enter backup name'
+$backupFolder = read-host 'Enter folder for backup'
+$timestamp = (Get-Date).ToString("yyyyMMddHHmm")
+$fullBackupName = "$backupFolder\" + $backupName + "_$timestamp.bak"
+$credential = read-host 'Enter user name'
+$params = $database,$computer,$fullBackupName
+
+$backupScript = { 
+    param([string[]]$params)
+    $database = $params[0]
+    $computer = $params[1]
+    $fullBackupName = $params[2]
+    Write-Host "Logged in to server $computer"
+    Write-Host "Server: $computer"
+    Write-Host "Database: $database"
+    Write-Host "Backup location: $fullBackupName"
+    import-Module SQLPS
+    Backup-SqlDatabase -ServerInstance $computer -Database $database -BackupFile $fullBackupName 
+}
+
+invoke-command -computername $computer -scriptblock $backupScript -ArgumentList (,$params) -Credential $credential
